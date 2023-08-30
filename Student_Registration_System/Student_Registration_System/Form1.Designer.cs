67e9d1ffa4c08a2d13ea45d77baf0ecc83bbe947
tammy9011 <85066644+tammy9011@users.noreply.github.com>
2023-08-30 13:30:10 +0200

    Add files via upload

---
+++ Student_Registration_System/Student_Registration_System/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Student_Registration_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtFathersName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dtDOB = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.classComboBox = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(47)))), ((int)(((byte)(78)))));
+            this.panel1.Controls.Add(this.cancelButton);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1200, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.cancelButton.FlatAppearance.BorderSize = 0;
+            this.cancelButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cancelButton.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelButton.ForeColor = System.Drawing.Color.White;
+            this.cancelButton.Location = new System.Drawing.Point(1097, 0);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(103, 100);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "X";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(43, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Registration System";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(47)))), ((int)(((byte)(78)))));
+            this.label2.Location = new System.Drawing.Point(281, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Student Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(446, 184);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(304, 27);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtFathersName
+            // 
+            this.txtFathersName.Location = new System.Drawing.Point(446, 241);
+            this.txtFathersName.Name = "txtFathersName";
+            this.txtFathersName.Size = new System.Drawing.Size(304, 27);
+            this.txtFathersName.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(47)))), ((int)(((byte)(78)))));
+            this.label4.Location = new System.Drawing.Point(281, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Father\'s Name:";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(446, 297);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(304, 27);
+            this.txtAddress.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(47)))), ((int)(((byte)(78)))));
+            this.label5.Location = new System.Drawing.Point(338, 297);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 23);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Address:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(47)))), ((int)(((byte)(78)))));
+            this.label6.Location = new System.Drawing.Point(302, 350);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(125, 23);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Date of Birth:";
+            // 
+            // dtDOB
+            // 
+            this.dtDOB.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtDOB.Location = new System.Drawing.Point(446, 350);
+            this.dtDOB.Name = "dtDOB";
+            this.dtDOB.Size = new System.Drawing.Size(304, 27);
+            this.dtDOB.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(47)))), ((int)(((byte)(78)))));
+            this.label7.Location = new System.Drawing.Point(338, 399);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 23);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Class:";
+            // 
+            // classComboBox
+            // 
+            this.classComboBox.FormattingEnabled = true;
+            this.classComboBox.Location = new System.Drawing.Point(446, 400);
+            this.classComboBox.Name = "classComboBox";
+            this.classComboBox.Size = new System.Drawing.Size(223, 27);
+            this.classComboBox.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 523);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1200, 226);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // saveButton
+            // 
+            this.saveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.saveButton.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.ForeColor = System.Drawing.Color.SeaGreen;
+            this.saveButton.Location = new System.Drawing.Point(586, 451);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(164, 43);
+            this.saveButton.TabIndex = 12;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1200, 749);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.classComboBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dtDOB);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtFathersName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtFathersName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dtDOB;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox classComboBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
+
---
+++ Student_Registration_System/Student_Registration_System/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SLRDbConnector;
+
+namespace Student_Registration_System
+{
+    public partial class Form1 : Form
+    {
+        DbConnector db;
+        public Form1()
+        {
+            InitializeComponent();
+            db = new DbConnector();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            db.FillCombobox("SELECT name FROM tblClasses", classComboBox);
+            db.fillDataGridView("SELECT * FROM tblStudents", dataGridView1);
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            string class_id;
+
+            if (isFormValid())
+            {
+                db.getSingleValue("SELECT id FROM tblClasses WHERE name= '" + classComboBox.Text.ToString() + "'", out class_id, 0);
+                string abc = db.performCRUD("insert into tblStudents(name,fathers_name,address,date_of_birth,class_id) values('" + txtName.Text + "','" + txtFathersName.Text + "','" + txtAddress.Text + "','" + dtDOB.Text + "','" + class_id + "')");
+                this.OnLoad(e);
+            }
+            else
+            {
+                MessageBox.Show("Please Fill All Required Fields", "Required Fields Are Empty",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private bool isFormValid()
+        {
+            if (txtName.Text.ToString().Trim() == string.Empty
+                || txtFathersName.Text.ToString().Trim() == string.Empty
+                || txtAddress.Text.ToString().Trim() == string.Empty)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            //exits form
+            Application.Exit();
+        }
+    }
+}
